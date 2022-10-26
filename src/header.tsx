81dda62b9005c0a90b8f6022b089ced896dc5159
zachchoi12 <zachwchoi@gmail.com>
2022-10-25 19:01:17 -0700

    Update Twitter URL

--- src/header.tsx
+++ src/header.tsx
@@ -5,7 +5,7 @@ export const Header = () => {
         <div className='mx-auto w-screen inset-x-0 fixed z-50 h-[0] px-[5vw]'>
             <div className='flex justify-between items-center pt-5 md:pt-10'>
                 <a className='justify-self-start' href='#'><img className='h-[44.625px] md:h-[63.75px]' src='/logo.svg' /></a>
-                <a className='justify-center' href='https://twitter.com/heistedd' target='_blank'><img className='h-[21px] md:h-[24px]' src='/twitter.svg' /></a>
+                <a className='justify-center' href='https://twitter.com/teamheist_io' target='_blank'><img className='h-[21px] md:h-[24px]' src='/twitter.svg' /></a>
             </div>
         </div>
     );
